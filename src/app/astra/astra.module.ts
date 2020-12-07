1b275f04895ca7c26fcf8504614fbdc1f13d1823
Fabien Dehopré <fabien.dehopre@fluxys.com>
2020-12-07 14:31:58 +0100

    style p-dialog

--- src/app/astra/astra.module.ts
+++ src/app/astra/astra.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { PanelModule } from 'primeng/panel';
-import {SplitButtonModule} from 'primeng/splitbutton';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { DialogModule } from 'primeng/dialog';
 
 import { AstraRoutingModule } from './astra-routing.module';
 import { AstraComponent } from './astra.component';
 import { ButtonsComponent } from './buttons/buttons.component';
 import { DialogsComponent } from './dialogs/dialogs.component';
-import {ConfirmDialogModule} from 'primeng/confirmdialog';
 
 
 @NgModule({
@@ -20,6 +21,7 @@ import {ConfirmDialogModule} from 'primeng/confirmdialog';
     PanelModule,
     SplitButtonModule,
     ConfirmDialogModule,
+    DialogModule,
   ]
 })
 export class AstraModule { }
--- src/app/astra/dialogs/dialogs.component.ts
+++ src/app/astra/dialogs/dialogs.component.ts
@@ -4,26 +4,34 @@ import { ConfirmationService } from 'primeng/api';
 @Component({
   selector: 'app-dialogs',
   templateUrl: './dialogs.component.html',
+  styles: [`.fake-content {
+    width: 450px;
+    height: 250px;
+  }`],
   providers: [ConfirmationService],
 })
 export class DialogsComponent {
+  neutralDialogVisible = false;
+  withinDayDialogVisible = false;
+  dayAheadDialogVisible = false;
+
   constructor(private readonly confirmationService: ConfirmationService) {}
 
-  confirmNormal(): void {
+  confirmNeutral(): void {
     this.confirmationService.confirm({
       key: 'normalCD',
       message: 'Dialog is a container to display content in an overlay window.',
     });
   }
 
-  confirmWD(): void {
+  confirmWithinDay(): void {
     this.confirmationService.confirm({
       key: 'withinDayCD',
       message: 'Dialog is a container to display content in an overlay window.',
     });
   }
 
-  confirmDA(): void {
+  confirmDayAhead(): void {
     this.confirmationService.confirm({
       key: 'dayAheadCD',
       message: 'Dialog is a container to display content in an overlay window.',
@@ -36,5 +44,28 @@ export class DialogsComponent {
       message: 'Dialog is a container to display content in an overlay window.',
       icon: 'pi pi-exclamation-circle'
     });
+  }
+
+  showNeutral(): void {
+    this.neutralDialogVisible = true;
+  }
+
+  showWithinDay(): void {
+    this.withinDayDialogVisible = true;
+  }
+
+  showDayAhead(): void {
+    this.dayAheadDialogVisible = true;
+  }
+
+  close(): void {
+    this.neutralDialogVisible = false;
+    this.withinDayDialogVisible = false;
+    this.dayAheadDialogVisible = false;
+  }
+
+  performAction(): void {
+    // do something
+    this.close();
   }
 }
