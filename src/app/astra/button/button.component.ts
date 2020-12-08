ed03314c7971736ecf23d02c6840481ed756a8c5
Fabien Dehopré <fabien.dehopre@fluxys.com>
2020-12-08 13:12:33 +0100

    fix style in button demo

--- src/app/astra/button/button.component.ts
+++ src/app/astra/button/button.component.ts
@@ -9,6 +9,10 @@ import { Component } from '@angular/core';
       p-button {
         margin: 0 0.5rem 0.5rem 0;
       }
+      /* reset the margin for button set */
+      .p-buttonset > .p-button {
+        margin: 0;
+      }
     `,
   ],
 })
