e37f843635a23c52aeaaa4aae6bea3d7e5b344a5
Fabien Dehopré <fabien.dehopre@fluxys.com>
2020-12-11 11:25:00 +0100

    style tabmenu

--- src/app/astra/tab-menu/tab-menu.component.ts
+++ src/app/astra/tab-menu/tab-menu.component.ts
@@ -15,7 +15,8 @@ export class TabMenuComponent implements OnInit {
       {label: 'Calendar', icon: 'pi pi-fw pi-calendar'},
       {label: 'Edit', icon: 'pi pi-fw pi-pencil'},
       {label: 'Documentation', icon: 'pi pi-fw pi-file'},
-      {label: 'Settings', icon: 'pi pi-fw pi-cog'}
+      {label: 'Settings', icon: 'pi pi-fw pi-cog'},
+      {label: 'Disabled', disabled: true}
     ];
 
     this.activeItem = this.items[0];
